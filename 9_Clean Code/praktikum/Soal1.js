14d8f2657e30ec86ca7b5a4ae79cc7c6a28a0028
derrydwi <derrydwiaditya@gmail.com>
2022-03-10 15:56:38 +0700

    update output soal 1 task section 9

--- 9_Clean Code/praktikum/Soal1.js
+++ 9_Clean Code/praktikum/Soal1.js
@@ -34,5 +34,5 @@ userService.addUser({ id: 2, username: 'Doe', password: '456' });
 userService.addUser({ id: 3, username: 'Mark', password: '789' });
 userService.addUser({ id: 4, username: 'Frank', password: '654' });
 
-console.log('Users List: ', userService.getAllUsers());
-console.log('User with Id 3: ', userService.getUserById(3));
+console.log('Users List:', userService.getAllUsers());
+console.log('User with Id 3:', userService.getUserById(3));
