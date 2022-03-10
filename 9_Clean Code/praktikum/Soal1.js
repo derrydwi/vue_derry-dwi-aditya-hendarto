dbca271b67fbf3b82f506c9810193cbe091b2e6e
derrydwi <derrydwiaditya@gmail.com>
2022-03-10 15:23:39 +0700

    fix function params with object & update ss task 9

--- 9_Clean Code/praktikum/Soal1.js
+++ 9_Clean Code/praktikum/Soal1.js
@@ -1,5 +1,5 @@
 class User {
-  constructor(id, username, password) {
+  constructor({ id, username, password }) {
     this.id = id;
     this.username = username;
     this.password = password;
@@ -12,8 +12,8 @@ class UserService {
   }
 
   // create user object, push to users array, and return the object
-  addUser(id, username, password) {
-    const user = new User(id, username, password);
+  addUser({ id, username, password }) {
+    const user = new User({ id, username, password });
     this.users.push(user);
     return user;
   }
@@ -29,10 +29,10 @@ class UserService {
 
 const userService = new UserService();
 
-userService.addUser(1, 'Jhon', '123');
-userService.addUser(2, 'Doe', '456');
-userService.addUser(3, 'Mark', '789');
-userService.addUser(4, 'Frank', '654');
+userService.addUser({ id: 1, username: 'Jhon', password: '123' });
+userService.addUser({ id: 2, username: 'Doe', password: '456' });
+userService.addUser({ id: 3, username: 'Mark', password: '789' });
+userService.addUser({ id: 4, username: 'Frank', password: '654' });
 
 console.log('Users List: ', userService.getAllUsers());
 console.log('User with Id 3: ', userService.getUserById(3));
