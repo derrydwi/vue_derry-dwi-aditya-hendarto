89c000b85a0a7322aff8aad26b540deca7645859
derrydwi <derrydwiaditya@gmail.com>
2022-03-21 15:35:51 +0700

    use direct instead lazy load detail route task 14

--- 14_Vue Helpers/praktikum/src/router/index.js
+++ 14_Vue Helpers/praktikum/src/router/index.js
@@ -1,29 +1,27 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import DetailView from '../views/DetailView.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView,
   },
   {
     path: '/:id',
     name: 'detail',
-    // route level code-splitting
-    // this generates a separate chunk (detail.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "detail" */ '../views/DetailView.vue')
-  }
-]
+    component: DetailView,
+  },
+];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
